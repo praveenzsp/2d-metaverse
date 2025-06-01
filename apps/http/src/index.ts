945ff52b4408905115e2535440f686caad1ef0f5
praveenzsp <psai6645@gmail.com>
2025-06-01 19:03:05 +0530

    added cors

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { router } from "./routes/v1";
-
+import cors from "cors";
 const app = express();
+
+app.use(cors({
+	origin: '*',
+}));
+
 app.use(express.json());
 
 app.use("/api/v1", router);
 
 
-app.listen(process.env.PORT || 3000, ()=>{
-	console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 8080, ()=>{
+	console.log("Server is running on port 8080");
 })
